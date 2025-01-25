d8f0ba3016a72f44d410c4e7107b80710bcc43df
mathi404 <mathijslanghorst@hotmail.com>
2025-01-25 14:04:53 +0100

    added some styling for the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import "./css/NavbarStyles.css"
 import { useState } from "react";
+import cartIcon from "../assets/grocery-store.png"
 function Navbar(){
     const [isNavActive , setIsNavActive] = useState(true)
 
@@ -10,16 +11,25 @@ function Navbar(){
       
     }
     return(
+        <div className="container">
+            <div className={`wrapper ${isNavActive ? "": "active"}`}>
+                <ul className="main-nav-ul">
+                    <li className="logo">Berserk Bazaar</li>
+                    <li><button className="nav-btn-shoppingCart"><img src={cartIcon} alt="Shopping Cart"></img></button></li>
+                    <li><button className="nav-btn-likedItems"><i className="fa fa-heart"></i></button></li>
+                </ul>
+                </div>
         <div className={`navbar-wrapper ${isNavActive ? "": "active"}`}>
             <div className="close-btn-wrapper">
             <button onClick={handleNavbar} className="close-btn">{isNavActive ? <i className="fa fa-bars"></i>: "x"}</button>
             </div>
             <ul className="ul">
-                <li><a>clothing</a></li>
-                <li><a>books</a></li>
-                <li><a>merchandise</a></li>
-                <li><a>toys</a></li>
+                <li><a>Clothing</a></li>
+                <li><a>Books</a></li>
+                <li><a>Merchandise</a></li>
+                <li><a>Toys</a></li>
             </ul>
+        </div>
         </div>
     )
 }
